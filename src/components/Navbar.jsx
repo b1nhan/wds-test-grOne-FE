5a190af0f2617ad8bdc5fd73b571cd94cddf43b3
Calvinan132 <trinhhoaian2303@gmail.com>
2026-01-03 01:26:10 +0700

    [FE] Đưa demo giao diện home

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import {Bell, CircleUser,ShoppingCart } from "lucide-react";
+
+let Navbar = () => { 
+    return <header> 
+        <div className="container-fluid w-full bg-white p-4 shadow flex justify-between items-center px-10 fixed z-10 ">
+            <div className="Left font-bold text-3xl">LOGO</div>
+            <div className="Right flex gap-6">
+                <Bell />
+                <ShoppingCart />
+                <div className="flex gap-2 items-center">
+                    <b >Username</b>
+                    <CircleUser className="size-8"/>
+                </div>
+            </div>
+         </div>
+    </header>
+        
+}
+export default Navbar;
